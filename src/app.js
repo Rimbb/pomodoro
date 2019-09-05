68c8182e3c5d4969958aab1f79e48f07334070d8
Rimbaud Gregoire <rimbaudgregoire@hotmail.be>
2019-09-05 16:21:21 +0200

    v2

--- src/app.js
+++ src/app.js
@@ -18,10 +18,8 @@ fetch(url)
         const rand2auth = rand2.author;
 
         return (
-            document.body.style.backgroundImage = `url("${rand2url}")`,
-            document.querySelector(
-                "#author",
-            ).innerHTML = `Photo by :&nbsp;&nbsp;<a href='${rand2urldisp}' target='blank'>${rand2auth}</a>`
+            (document.body.style.backgroundImage = `url("${rand2url}")`),
+            (document.querySelector("#author").innerHTML = `Photo by :&nbsp;&nbsp;<a href='${rand2urldisp}' target='blank'>${rand2auth}</a>`)
         );
     });
 
@@ -120,26 +118,39 @@ class App extends React.Component {
             />
         );
         const stop = !this.state.isOn ? null : (
-            <Bouton id={"stop"} value={"../src/assets/stop.png"} handleFunction={this.stop} />
+            <Bouton
+                id={"stop"}
+                value={"../src/assets/stop.png"}
+                handleFunction={this.stop}
+            />
         );
         const add =
             s !== "00" || this.state.isOn ? null : (
-                <Bouton value={"../src/assets/add.png"} handleFunction={this.plus} />
+                <Bouton
+                    value={"../src/assets/add.png"}
+                    handleFunction={this.plus}
+                />
             );
         const minus =
             s !== "00" || this.state.isOn ? null : (
-                <Bouton value={"../src/assets/minus.png"} handleFunction={this.moins} />
+                <Bouton
+                    value={"../src/assets/minus.png"}
+                    handleFunction={this.moins}
+                />
             );
         const reset =
-            s == "00" ||
-            this.state.seconds == 1500000 ||
+            s === "00" ||
+            this.state.seconds === 1500000 ||
             this.state.isOn ? null : (
-                <Bouton value={"../src/assets/reset.png"} handleFunction={this.reset} />
+                <Bouton
+                    value={"../src/assets/reset.png"}
+                    handleFunction={this.reset}
+                />
             );
         return (
             <div id={"wrapper"}>
                 <div id={"one"}>
-                    <h5>What's your focus for this run?</h5>
+                    <h5>{"What's your focus for this run?"}</h5>
                     <div id={"focus"}>
                         <input className={"checkbox"} type={"checkbox"} />
                         <input className={"text"} type={"text"} />
@@ -158,10 +169,10 @@ class App extends React.Component {
                     </div>
                 </div>
                 <div id={"onetwo"}>
-                    <h5>This is what you've already done</h5>
+                    <h5>{"This is what you've already done"}</h5>
                 </div>
                 <div id={"two"}>
-                    <h5>Your Pomodoro</h5>
+                    <h5>{"Your Pomodoro"}</h5>
                 </div>
                 <div id={"three"}>
                     <Timer seconds={finaltimer} />
